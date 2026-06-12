721c96b6016e4eabcad3392e059035fc166e0aca
JoaoLopes06519 <shark06519@sharkcoders.pt>
2026-06-12 20:03:28 +0100

    leitor de musica.py

--- leitor de musica.py
+++ leitor de musica.py
@@ -1,5 +1,6 @@
+import io
 from tkinter import *
-#import pygame
+import pygame
 import requests
 
 root = Tk()
@@ -7,18 +8,46 @@ root.title ("Leitor de Música")
 root.geometry("500x300")
 
 def chamar_api():
-	url = "https://api.deezer.com/search?q='Foo Fighters'"
+	url = "https://api.deezer.com/search?q='Within Temptation'"
 
 	# A GET request to the API
 	response = requests.get(url)
 	data = response.json()
 	dados = data["data"][0]
 	dados2 = dados["preview"]
+	musica = dados["title"]
+	artista = dados["artist"]["name"]
+	album = dados["album"]["title"]
 
 	print(dados2)
 
 	# Print the response
 	print(response.json())
+
+
+	# 2. Descarregar o ficheiro de áudio em formato de bytes
+	resposta_audio = requests.get(dados2)
+
+	# Transformar os bytes num "objeto semelhante a um ficheiro" na memória
+	ficheiro_audio_memoria = io.BytesIO(resposta_audio.content)
+
+	# 3. Inicializar o Pygame e reproduzir
+	pygame.mixer.init()
+
+	# O pygame.mixer.music consegue ler objetos do tipo BytesIO diretamente!
+	pygame.mixer.music.load(ficheiro_audio_memoria)
+
+	print("\nA reproduzir a amostra! Prime Ctrl+C para parar.")
+	pygame.mixer.music.play()
+
+	# Manter o programa aberto enquanto a música toca
+	import time
+
+	while pygame.mixer.music.get_busy():
+		time.sleep(1)
+
+
+
 
 
 chamar_api()
